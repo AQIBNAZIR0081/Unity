86f70eb4cc238cbafe225e115706e96fa385d8c0
AQIBNAZIR0081 <144234362+AQIBNAZIR0081@users.noreply.github.com>
2025-05-15 11:08:49 +0500

    updated movement and make build

--- MultiplayerGame/Assets/PlayerMovement.cs
+++ MultiplayerGame/Assets/PlayerMovement.cs
@@ -5,11 +5,6 @@ using Photon.Pun;
 
 public class Player : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
