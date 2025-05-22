b1ee67053f81ffe4dfbe2289cdbe31094b65d223
AQIBNAZIR0081 <144234362+AQIBNAZIR0081@users.noreply.github.com>
2025-05-22 09:13:36 +0500

    initial Commit

---
+++ PuzzleGame2D/Assets/RotationSprite.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RotationSprite : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int[] rotationAngle = { -90, 90, 180, -180 };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
