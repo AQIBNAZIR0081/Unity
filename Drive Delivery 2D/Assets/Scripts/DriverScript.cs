a35415ca37eaec0cf23090ef3b9197a43a6d2be1
AQIBNAZIR0081 <144234362+AQIBNAZIR0081@users.noreply.github.com>
2025-06-11 17:16:26 +0500

    create instance of Drive script

--- Drive Delivery 2D/Assets/Scripts/DriverScript.cs
+++ Drive Delivery 2D/Assets/Scripts/DriverScript.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class DriverScript : MonoBehaviour
 {
+    // Singleton instance of DriverScript
+    public static DriverScript Instance { get; private set; }
+
     [SerializeField] float steerSpeed = 100.0f;
     [SerializeField] float moveSpeed = 15.0f;
     
     bool isMoving;
+
+    private void Awake()
+    {
+        // Ensure only one instance of DriverScript exists
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
